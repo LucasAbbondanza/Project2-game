ebba35b2f8408c5dc831ff4cec14cf8c4415e252
nebal barhoome <nebalasaad@hotmail.com>
2018-05-04 10:19:28 -0400

    create spawners for enemies and upload the stars and hearts

--- app/src/main/java/walker/World.java
+++ app/src/main/java/walker/World.java
@@ -8,18 +8,57 @@ import com.lucasabbondanza.android.project2.walker.R;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
+import static walker.GameActivity.gameOnsound;
+import static walker.PlayerSprite.dead;
 
 class World {
+
     private List<Sprite> sprites;
     private PlayerSprite player;
+    private int position_y;
+    private double timer;
+
+    private BitmapSequence game_over;
+
+    private int enemy_count = 2;
+    private int enemy_killed = 0;
 
     public World() {
         sprites = new ArrayList<>();
         sprites.add(player = new PlayerSprite(new Vec2d(200,500)));
-        sprites.add(new AlienSprite(new Vec2d(3000, 100)));
-        sprites.add(new AlienSprite(new Vec2d(3000, 550)));
-        sprites.add(new AlienSprite(new Vec2d(3000, 1000)));
-        //sprites.add(new AlienSprite(new Vec2d(700, 100)));
+        sprites.add( new HeartSprite(new Vec2d(2600,0)));
+        sprites.add( new HeartSprite(new Vec2d(2400,0)));
+        sprites.add( new HeartSprite(new Vec2d(2200,0)));
+
+        //spawnEnemy();
+    }
+
+    private void create_enemy(int enemy_count){
+        Random rand = new Random();
+        float x = player.getPosition().getX();
+        int  position_y = rand.nextInt(2000) + 500;
+        for (int i=1 ;i<= enemy_count; i++){
+            sprites.add(new AlienSprite(new Vec2d(x + 3000,  (position_y * i) /2)));
+        }
+    }
+
+    private void create_stars(){
+        int stars_count = 2;
+        Random rand = new Random();
+        float x = player.getPosition().getX();
+        int  position_y = rand.nextInt(2000) + 500;
+        for (int i=1 ;i<= stars_count; i++){
+            sprites.add(new StarSpirte(new Vec2d(x + 3000,  (position_y * i) /2)));
+        }
+    }
+
+    private void spawnEnemy() {
+        float x = player.getPosition().getX();
+        sprites.add(new AlienSprite(new Vec2d(x + 3000 , 100)));
+        sprites.add(new AlienSprite(new Vec2d(x + 3000 , 550)));
+        sprites.add(new AlienSprite(new Vec2d(x + 3000 , 1000)));
     }
 
     public void tick(double dt) {
@@ -28,6 +67,13 @@ class World {
             handleMotionEvent(e);
         for(Sprite s: sprites)
             s.tick(dt);
+        timer += dt;
+        if (timer > 0){
+            timer = -1;
+            create_stars();
+            create_enemy(enemy_count);
+            //spawnEnemy();
+        }
         resolveCollisions();
     }
 
@@ -37,11 +83,9 @@ class World {
             for(int j=i+1; j < sprites.size(); j++) {
                 Sprite s1 = sprites.get(i);
                 Sprite s2 = sprites.get(j);
-
                 if (s1.collidesWith(s2))
                     collisions.add(new Collision(s1, s2));
             }
-
         for(Collision c: collisions) c.resolve();
     }
 
@@ -53,6 +97,7 @@ class World {
      * @param e the MotionEvent corresponding to the touch
      */
     private void handleMotionEvent(MotionEvent e) {
+
 
     }
 
@@ -66,5 +111,19 @@ class World {
         c.drawBitmap(bg, bg.getWidth()*(backgroundNumber+1), 0,  null);
         for(Sprite s: sprites)
             s.draw(c);
+
+        //position_y = (int) x;
+        if(dead){
+            BitmapRepo r = BitmapRepo.getInstance();
+            game_over = new BitmapSequence();
+            game_over.addImage(r.getImage(R.drawable.gameover), 100);
+            player.setBitmaps(game_over);
+            gameOnsound.stop();
+
+
+            //gameOversound.start();
+        }
     }
+
 }
+
