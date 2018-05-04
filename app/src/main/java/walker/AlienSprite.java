4fde382b678d1791a40722a92bf9c3a97631a4c3
nebal barhoome <nebalasaad@hotmail.com>
2018-05-04 10:15:17 -0400

    change position velocityX

--- app/src/main/java/walker/AlienSprite.java
+++ app/src/main/java/walker/AlienSprite.java
@@ -7,7 +7,6 @@ public class AlienSprite extends Sprite {
     private static final int velocityX = -300;
     private static final int velocityY = 0;
 
-
     public AlienSprite(Vec2d p) {
         super(p);
         loadBitmaps();
@@ -17,7 +16,7 @@ public class AlienSprite extends Sprite {
         BitmapRepo r = BitmapRepo.getInstance();
         BitmapSequence s = new BitmapSequence();
         s.addImage(r.getImage(R.drawable.enemy), 0.1);
-        s.addImage(r.getImage(R.drawable.enemy), 0.1);
+        //s.addImage(r.getImage(R.drawable.alien), 0.1);
         setBitmaps(s);
 
     }
@@ -27,6 +26,7 @@ public class AlienSprite extends Sprite {
     public void tick(double dt) {
         super.tick(dt);
         setPosition(getPosition().add(new Vec2d(velocityX*dt,velocityY*dt)));
+
     }
 
     @Override
