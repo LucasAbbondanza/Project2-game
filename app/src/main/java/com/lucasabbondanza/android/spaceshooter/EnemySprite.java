3e4a8643f10c4e27ae0520625734dc36615b85af
LucasAbbondanza <38789039+LucasAbbondanza@users.noreply.github.com>
2018-05-07 17:12:48 -0400

    Changes to EnemySprite:    - Changed enemy sprite    - Added death animation (same as player)    - Added death in general (may need to limit death to specific causes)

--- app/src/main/java/com/lucasabbondanza/android/spaceshooter/EnemySprite.java
+++ app/src/main/java/com/lucasabbondanza/android/spaceshooter/EnemySprite.java
@@ -2,9 +2,12 @@ package com.lucasabbondanza.android.spaceshooter;
 
 public class EnemySprite extends Sprite {
 
+    private static BitmapSequence deadSequence;
+
     private int xVelocity;
     private int yVelocity;
     private double time;
+    private boolean dead;
 
 
     EnemySprite(Vec2d p) {
@@ -12,14 +15,29 @@ public class EnemySprite extends Sprite {
         loadBitmaps();
         xVelocity = 0;
         yVelocity = 1000;
+        dead = false;
     }
 
     private void loadBitmaps() {
         BitmapRepo r = BitmapRepo.getInstance();
         BitmapSequence s = new BitmapSequence();
         s.addImage(r.getImage(R.drawable.enemy), 0.1);
-        setBitmaps(s);
 
+        deadSequence = new BitmapSequence();
+        deadSequence.addImage(r.getImage(R.drawable.boom_frame_00), 0.01);
+        deadSequence.addImage(r.getImage(R.drawable.boom_frame_01), 0.01);
+        deadSequence.addImage(r.getImage(R.drawable.boom_frame_02), 0.01);
+        deadSequence.addImage(r.getImage(R.drawable.boom_frame_03), 0.01);
+        deadSequence.addImage(r.getImage(R.drawable.boom_frame_04), 0.01);
+        deadSequence.addImage(r.getImage(R.drawable.boom_frame_05), 0.01);
+        deadSequence.addImage(r.getImage(R.drawable.boom_frame_06), 0.01);
+        deadSequence.addImage(r.getImage(R.drawable.boom_frame_07), 0.01);
+        deadSequence.addImage(r.getImage(R.drawable.boom_frame_08), 0.01);
+        deadSequence.addImage(r.getImage(R.drawable.boom_frame_09), 0.01);
+        deadSequence.addImage(r.getImage(R.drawable.boom_frame_10), 0.01);
+        deadSequence.addImage(r.getImage(R.drawable.boom_frame_11), 0.01);
+        deadSequence.addImage(r.getImage(R.drawable.blank), 100000);
+        setBitmaps(s);
     }
 
     private void path() {
@@ -37,8 +55,25 @@ public class EnemySprite extends Sprite {
         return false;
     }
 
+    @Override
+    public void resolve(Collision collision, Sprite other) {
+        if (!dead) makeDead();
+    }
+
     public void setVelocity(float x, float y) {
 
+    }
+
+    public void makeDead() {
+        dead = true;
+        xVelocity = 0;
+        yVelocity = 0;
+        setBitmaps(deadSequence);
+    }
+
+    @Override
+    public boolean isDead() {
+        return dead;
     }
 
 }
