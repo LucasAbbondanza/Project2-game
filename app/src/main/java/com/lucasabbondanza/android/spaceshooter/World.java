9a7f9d9819f95ff39626b9dead74208f81568d59
LucasAbbondanza <38789039+LucasAbbondanza@users.noreply.github.com>
2018-05-07 17:29:24 -0400

    Changes to World:    - List delete: used for removing sprites from List sprites    - World now passes its TextureView to player    - Player now shoots once every other tick    - Bullet position is now placed relative to the player's bounding box,    rather than a set distance from the player's origin    - World now removes sprites when they are off screen, or if it is a    bullet that is "dead"    - Made it so the player shooting is only changed on ACTION_DOWN which    make tapping the screen toggle shooting    - Changed the background to galaxy1.gif(not animated)    - Background now extends past the viewport properly

--- app/src/main/java/com/lucasabbondanza/android/spaceshooter/World.java
+++ app/src/main/java/com/lucasabbondanza/android/spaceshooter/World.java
@@ -2,24 +2,43 @@ package com.lucasabbondanza.android.spaceshooter;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.view.TextureView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 class World {
     private List<Sprite> sprites;
+    private List<Sprite> delete;
     private PlayerSprite player;
+    private TextureView view;
 
-    public World() {
+    private int shootTick;
+
+    public World(TextureView textureView) {
+        view = textureView;
         sprites = new ArrayList<>();
+        delete = new ArrayList<>();
+        shootTick = 0;
         sprites.add(new EnemySprite(new Vec2d(100, 100)));
         sprites.add(new EnemySprite(new Vec2d(1000, 100)));
         //The player needs to be the last sprite added so it is drawn on top of most things
-        sprites.add(player = new PlayerSprite(new Vec2d(500,2000)));
+        sprites.add(player = new PlayerSprite(new Vec2d(500,2000), view));
     }
 
     public void tick(double dt) {
+        if(player.isShooting() && !player.isDead()) {
+            shootTick++;
+            if(shootTick%2 == 0) {
+                Bullet pBullet = new Bullet(new Vec2d(0,0));
+                float x = player.getPosition().getX()+(player.getBoundingBox().width()/2)-pBullet.getBoundingBox().width();
+                float y = player.getPosition().getY();
+                sprites.add(new Bullet(new Vec2d(x, y)));
+                shootTick = 0;
+            }
+        }
         MotionEvent e = TouchEventQueue.getInstance().dequeue();
         if (e != null)
             handleMotionEvent(e);
@@ -27,6 +46,22 @@ class World {
             s.tick(dt);
         }
         resolveCollisions();
+        for(Sprite s: sprites) {
+            if(s instanceof Bullet && s.isDead()) {
+                delete.add(s);
+            }else if(s.getPosition().getY() < player.getPosition().getY()-3000) {
+                delete.add(s);
+            }else if (s.getPosition().getY() > player.getPosition().getY()+1000){
+                delete.add(s);
+            }
+        }
+        if(!delete.isEmpty() && !player.isDead()) {
+            for (Sprite s : delete) {
+                Log.d("Sprite Cleanup", "Removing " + s);
+                sprites.remove(s);
+            }
+            delete.clear();
+        }
     }
 
     private void resolveCollisions() {
@@ -44,30 +79,26 @@ class World {
     }
 
     private void handleMotionEvent(MotionEvent e) {
-        if(e.getY() > 1200) {
+        if(e.getY() > view.getHeight()/2) {
             if (e.getActionMasked() == MotionEvent.ACTION_UP)
                 player.stop();
             else {
                 player.move(e.getX());
             }
-        } else if (e.getY() <= 1200){
-            player.fire(!player.isShooting());
-            while(e.getActionMasked() == MotionEvent.ACTION_DOWN){
-                float x = player.getPosition().getX() + 50;
-                float y = player.getPosition().getY();
-                sprites.add(new Bullet(new Vec2d(x, y)));
-            }
-
-
+        } else {
+            if (e.getActionMasked() == MotionEvent.ACTION_DOWN)
+                player.fire(!player.isShooting());
         }
     }
 
     public void draw(Canvas c) {
-        Bitmap bg = BitmapRepo.getInstance().getImage(R.drawable.background);
+        Bitmap bg = BitmapRepo.getInstance().getImage(R.drawable.galaxy1);
         float y = player.getPosition().getY();
         if(!player.isDead())
             c.translate(0, -y+2000);
         int backgroundNumber = (int)(y / bg.getHeight());
+        c.drawBitmap(bg, 0, bg.getHeight()*(backgroundNumber-3),  null);
+        c.drawBitmap(bg, 0, bg.getHeight()*(backgroundNumber-2),  null);
         c.drawBitmap(bg, 0, bg.getHeight()*(backgroundNumber-1),  null);
         c.drawBitmap(bg, 0, bg.getHeight()*backgroundNumber,  null);
         c.drawBitmap(bg, 0, bg.getHeight()*(backgroundNumber+1),  null);
